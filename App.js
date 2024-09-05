2665fa9f210f27de03b5806bea8aada52ada316c
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-09-05 09:34:15 +0300

    initial

--- App.js
+++ App.js
@@ -1,46 +1,23 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+import Code001 from './src/001_firstAnimation';
+import Code002 from './src/002_useAnimatedStyle';
+import Code003 from './src/003_useAnimatedProps';
+import Code004 from './src/004_withTiming';
+import Code005 from './src/005_withSpring';
+import Code006 from './src/006_withRepeat';
+import Code007 from './src/007_withSequence';
+import Code008 from './src/008_withDelay';
+import Code009a from './src/009_gestureHandler_tap';
+import Code009b from './src/009_gestureHandler_pan';
+import Code009c from './src/009_withDecay';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
-  );
+export default function App() {
+  //return <Code001 />
+  //return <Code002 />
+  //return <Code003 />
+  //return <Code004 width={100} />
+  //return <Code005 width={100} />
+  //return <Code006 />
+  //return <Code007 />
+  //return <Code008 />
+  return <Code009c />
 }
